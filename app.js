9d4f6a02678bb47325c66c6fb1d8466d428a3c52
Aziz Nabizada <aziz.cs2019@gmail.com>
2023-02-14 20:39:57 -0800

    error handler created"

--- app.js
+++ app.js
@@ -1,38 +1,27 @@
-// const express = require("express");
-// const app = express();
-// const tasks = require("./routes/tasks");
-// const connectDB = require("./db/connect");
-// require("dotenv").config();
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+require("dotenv").config();
 
-// app.use(express.static("./public"));
-// app.use(express.json());
-// app.use("/api/v1/tasks", tasks);
+app.use(express.static("./public"));
+app.use(express.json());
+app.use("/api/v1/tasks", tasks);
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-// const port = 3000;
-// const start = async () => {
-//   try {
-//     await connectDB(process.env.MONGO_URI);
-//     app.listen(port, () => console.log(`App is runing on port ${port} ...`));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-// start();
-
-const http = require("http");
-
-const server = http.createServer(function (req, res) {
-  if (req.url === "/") {
-    res.end("Home page for http://localhost");
-  } else if (req.url === "/about") {
-    res.end("This is about page");
-  } else {
-    res.end(`
-    THis page is not found please go back in home page
-    <a type="button" href="/" >Back Home</a>
-    `);
+const port = 3000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log(`App is runing on port ${port} ...`));
+  } catch (error) {
+    console.log(error);
   }
-});
+};
 
-server.listen(5000);
+start();
+
+
